99a56231b79c51f715b14872f8e69d3881ebc4db
Halil Bilgin <bilginhalil@gmail.com>
2017-05-02 09:13:26 +0300

    GLM

--- simulatedAnnealing.R
+++ simulatedAnnealing.R
@@ -1,14 +1,13 @@
 source('autoload.R')
 
-knnSA <- caretSA
 
-ctrl <- safsControl(functions = knnSA,
+ctrl <- safsControl(functions = caretSA,
                     method = "repeatedcv",
                     repeats = 10,
                     number=5,
                     ## What should we optimize? 
-                    metric = c(internal = "RMSE",
-                               external = "RMSE"),
+                    metric = c(internal = "AIC",
+                               external = "AIC"),
                     maximize = c(internal = FALSE,
                                  external = FALSE),
                     improve = 25,
@@ -17,9 +16,11 @@ knn_groups <- lapply(genderDb, function(gdb) {
   safs(x = gdb[, get('inputCols')[-2]],
        y = gdb$DEXAyagyuz,
        iters = 500,
-       safsControl = ctrl)
+       safsControl = ctrl,
+       method='glm'
+       )
 })
 
 
-save(knn_groups, file='knn_sa.RData')
+save(knn_groups, file='glm_sa.RData')
 
