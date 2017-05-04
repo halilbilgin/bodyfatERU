fd6c7c1f3ca0c73459b1131108950ce9ac00f22c
Halil Bilgin <bilginhalil@gmail.com>
2017-05-04 15:25:56 +0300

    new style

---
+++ cmdMethods.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+library("optparse")
+
+allMethods <- read.csv('allMethods.txt')
+option_list = list(
+  make_option(c("-m", "--methods"), type="character", default=1:3),
+  make_option(c("-s", "--seeds"), type="character", default=1:50)
+); 
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+methods <- allMethods[eval(parse(text = opt$methods)), 1]
+seeds <- eval(parse(text=opt$seeds))
+
+methodsTest(methods, seeds, paste(methods,'-',seeds))
--- methodsTest.R
+++ methodsTest.R
@@ -1,155 +1,97 @@
-source('autoload.R')
-
-methods <- c(
-        'glm',
-        'brnn',
-        'ppr',
-        'kernelpls',
-        'pls',
-        'pcr',
-        'bridge',
-        'gaussprLinear',
-        'gaussprPoly',
-        'bayesglm',
-        'svmLinear',
-        'knn',
-        'enet',
-        'bagEarthGCV',
-        'blassoAveraged',
-        'spikeslab',
-        'blasso',
-        'svmPoly',
-        'ridge',
-        'evtree',
-        'cubist',
-        'icr',
-        'lars',
-        'lm',
-        'rqnc',
-        'nnls',
-        'penalized',
-        'qrnn',
-        'rqlasso',
-        'krlsRadial',
-        'relaxo',
-        'rlm',
-        'lasso',
-        'gamboost',
-        'glmboost',
-        'BstLm',
-        'bstSm',
-        'blackboost',
-        'rpart',
-        'cforest',
-        'ctree',
-        'randomGLM',
-        'xgbLinear',
-        'elm',
-        'gaussprRadial',
-        'gamLoess',
-        'bam',
-        'mlpWeightDecay',
-        'rbf',
-        'ranger',
-        'RRF',
-        'xyf',
-        'dnn',
-        'svmRadial',
-        'nodeHarvest'
-        )
-
-#initialize formulas list
-load('rfGA30-100.RData')
-load('rf_sa.RData')
-fms <- list(
-  `1`=list(
-    `All`=toFormula(inputCols[-2]),
-    `SimulatedAnnealing`= toFormula(rf_sa_groups$`1`$optVariables),
-    `GeneticAlgorithm`=toFormula(rf_groups$`1`$optVariables),
-    `forward`=toFormula(c('kilo', 'cev_kalca', 'dkk_biceps', 
-                          'dkk_quadriceps')),
-    `backward`=toFormula(c('cev_kalca', 'cev_uyluk', 'cev_elblegi',
-                           'cev_kulacuzun', 'dkk_triceps', 'dkk_sscapula',
-                           'dkk_quadriceps'))
-  ),
-  `2`=list(
-    `All`=toFormula(inputCols[-2]),
-    `SimulatedAnnealing`=toFormula(rf_sa_groups$`2`$optVariables),
-    `GeneticAlgorithm`=toFormula(rf_groups$`2`$optVariables),
-    `forward`=toFormula(c('kilo', 'cev_bel', 'dkk_triceps',
-                          'dkk_sscapula', 'dkk_quadriceps', 'dkk_gogus')),
-    `backward`=toFormula(c('cev_boyun', 'cev_bel', 'cev_kulacuzun',
-                           'dkk_sscapula', 'dkk_silliak',
-                           'dkk_abdomen', 'dkk_quadriceps',
-                           'dkk_gogus'))
+methodsTest <- function(methods, trSplitSeeds, fileSuffix) {
+  source('autoload.R')
+  #initialize formulas list
+  load('rfGA30-100.RData')
+  load('rf_sa.RData')
+  fms <- list(
+    `1`=list(
+      `All`=toFormula(inputCols[-2]),
+      `SimulatedAnnealing`= toFormula(rf_sa_groups$`1`$optVariables),
+      `GeneticAlgorithm`=toFormula(rf_groups$`1`$optVariables),
+      `forward`=toFormula(c('kilo', 'cev_kalca', 'dkk_biceps', 
+                            'dkk_quadriceps')),
+      `backward`=toFormula(c('cev_kalca', 'cev_uyluk', 'cev_elblegi',
+                             'cev_kulacuzun', 'dkk_triceps', 'dkk_sscapula',
+                             'dkk_quadriceps'))
+    ),
+    `2`=list(
+      `All`=toFormula(inputCols[-2]),
+      `SimulatedAnnealing`=toFormula(rf_sa_groups$`2`$optVariables),
+      `GeneticAlgorithm`=toFormula(rf_groups$`2`$optVariables),
+      `forward`=toFormula(c('kilo', 'cev_bel', 'dkk_triceps',
+                            'dkk_sscapula', 'dkk_quadriceps', 'dkk_gogus')),
+      `backward`=toFormula(c('cev_boyun', 'cev_bel', 'cev_kulacuzun',
+                             'dkk_sscapula', 'dkk_silliak',
+                             'dkk_abdomen', 'dkk_quadriceps',
+                             'dkk_gogus'))
+    )
   )
-)
+  
+  
+  ## initialize result data frame
+  result <- data.frame(
+    gender=character(),
+    seed=numeric(),
+    method=character(),
+    fm=character(),
+    RMSE=numeric(),
+    MAPE=numeric(),
+    RSquared=numeric(),
+    Spearman=numeric()
+  )
+  
+  trControlSeeds <- getTrControlSeeds()
 
-
-## initialize result data frame
-result <- data.frame(
-  gender=character(),
-  seed=numeric(),
-  method=character(),
-  fm=character(),
-  RMSE=numeric(),
-  MAPE=numeric(),
-  RSquared=numeric(),
-  Spearman=numeric()
-)
-
-trControlSeeds <- getTrControlSeeds()
-load("trSplitSeeds.RData") 
-
-
-for(genderDbID in names(genderDb)) {
-  #length is = (n_repeats*nresampling)+1
-
-  for(trSplitSeed in trSplitSeeds) {
-    set.seed(trSplitSeed)
-    tt <- trTest(genderDb[[genderDbID]], p=.7)
+  for(genderDbID in names(genderDb)) {
+    #length is = (n_repeats*nresampling)+1
     
-    for(method in methods) {
-      for(fmsForGender in fms[genderDbID]) {
-        for(fmName in names(fmsForGender)) {
-          fm <- fmsForGender[[fmName]]
-          cat(paste('Gender:', genderDbID, 'Training:', method, 'Formula:', fmName, "\n"))
-          
-          fit <- try(train(as.formula(fm), data=tt$train, method=method,
-                           trControl=trainControl(method = 'repeatedcv',
-                                                  number=5,
-                                                  seeds=trControlSeeds,
-                                                  repeats=10)), TRUE)
-          if(inherits(fit, "try-error")){
-            cat(paste('Error..:', fit))
-            cat('\n')
-            next
+    for(trSplitSeed in trSplitSeeds) {
+      set.seed(trSplitSeed)
+      tt <- trTest(genderDb[[genderDbID]], p=.7)
+      
+      for(method in methods) {
+        for(fmsForGender in fms[genderDbID]) {
+          for(fmName in names(fmsForGender)) {
+            fm <- fmsForGender[[fmName]]
+            cat(paste('Gender:', genderDbID, 'Training:', method, 'Formula:', fmName, "\n"))
+            
+            fit <- try(train(as.formula(fm), data=tt$train, method=method,
+                             trControl=trainControl(method = 'repeatedcv',
+                                                    number=5,
+                                                    seeds=trControlSeeds,
+                                                    repeats=10)), TRUE)
+            if(inherits(fit, "try-error")){
+              cat(paste('Error..:', fit))
+              cat('\n')
+              next
+            }
+            
+            predicted <- predict(fit, newdata=tt$test)
+            error <- tt$test$DEXAyagyuz - predicted
+            curResult <- data.frame(
+              gender=genderDbID,
+              seed=trSplitSeed,
+              method=method,
+              fm=fmName ,
+              RMSE=rmse(error),
+              MAPE=mape(tt$test$DEXAyagyuz, predicted),
+              RSquared=rSquared(tt$test$DEXAyagyuz, predicted),
+              Spearman=rcorr(tt$test$DEXAyagyuz, predicted, type="spearman")$r[1,2],
+              MAE=mae(error)
+            )
+            
+            result <- rbind(result, curResult)
+            remove(fit)
+            gc()
+            print(curResult)
           }
-          
-          predicted <- predict(fit, newdata=tt$test)
-          error <- tt$test$DEXAyagyuz - predicted
-          curResult <- data.frame(
-            gender=genderDbID,
-            seed=trSplitSeed,
-            method=method,
-            fm=fmName ,
-            RMSE=rmse(error),
-            MAPE=mape(tt$test$DEXAyagyuz, predicted),
-            RSquared=rSquared(tt$test$DEXAyagyuz, predicted),
-            Spearman=rcorr(tt$test$DEXAyagyuz, predicted, type="spearman")$r[1,2],
-            MAE=mae(error)
-          )
-          
-          result <- rbind(result, curResult)
-          remove(fit)
-          gc()
-          print(curResult)
         }
       }
     }
+    
   }
   
+  save(result, file=paste('result-', fileSuffix, '.RData', sep = ''))
 }
 
-x<- trSplitSeeds
-save(result, file=paste('result-', x[1], '-', x[length(x)], '.RData', sep = ''))
+
