520c487e12294caba3ce580e126617b54a92e93b
root <bilginhalil@gmail.com>
2017-06-10 17:28:46 +0300

    update command line file

--- cmdMethods.R
+++ cmdMethods.R
@@ -3,13 +3,15 @@ library("optparse")
 source('methodsTest.R')
 allMethods <- read.csv('allMethods.txt')
 option_list = list(
-  make_option(c("-m", "--methods"), type="character", default="1:3"),
-  make_option(c("-s", "--seeds"), type="character", default="1:50")
+  make_option(c("-m", "--methods"), type="character", default="glm"),
+  make_option(c("-c", "--cores"), type='numeric', default=1)
 ); 
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
-methods <- allMethods[eval(parse(text = opt$methods)), 1]
-seeds <- eval(parse(text=opt$seeds))
+library(doParallel)
+registerDoParallel(cores=opt$cores)
+#methods <- allMethods[eval(parse(text = opt$methods)), 1]
+#seeds <- eval(parse(text=opt$seeds))
 
-methodsTest(methods, seeds, paste(opt$methods,'-',opt$seeds, sep = ''))
+methodsTest(strsplit(opt$methods, '-'), fileSuffix=opt$methods)
